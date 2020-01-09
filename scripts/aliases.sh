c9cc0aa08aca2cbee4b18088779558491fc29512
kaushikchandrashekar <kaushik.c02@gmail.com>
2020-01-09 11:10:01 +0530

    Some more aliases

--- scripts/aliases.sh
+++ scripts/aliases.sh
@@ -8,7 +8,7 @@ alias whats-my-ip='curl ifconfig.me'
 
 alias unset-aws-dynamic-creds='unset AWS_ACCESS_KEY_ID; unset AWS_SECRET_ACCESS_KEY; unset AWS_SESSION_TOKEN'
 
-alias sync-time='sudo service ntp stop; sleep 10; sudo service ntp start; sleep 10; sudo service ntp status'
+alias sync-time='echo "ntp service is $(sudo systemctl is-active ntp)"; sudo systemctl stop ntp; sleep 10; sudo systemctl start ntp; sleep 10; echo "ntp service is $(sudo systemctl is-active ntp)";'
 
 alias local-server='python3 -m http.server'
 
@@ -30,3 +30,5 @@ alias tf-state-show='tf state show'
 alias tf-state-rm='tf state rm'
 alias tf-import='tf import'
 alias tf-fmt='tf fmt'
+alias tf-taint='tf taint'
+alias tf-untaint='tf untaint'
